0f9d099e5e941fac7546a1263928c9380fd1f6b7
Saif64 <saifmahmud81@gmail.com>
2021-11-03 23:51:46 +0600

    added coroutines and coil

--- app/src/main/java/com/saif/mealz/ui/model/MealsRepository.kt
+++ app/src/main/java/com/saif/mealz/ui/model/MealsRepository.kt
@@ -1,12 +1,9 @@
 package com.saif.mealz.ui.model
 
-import com.saif.mealz.ui.model.api.MealsWebService
+import com.saif.mealz.ui.model.api.MealApi
 import com.saif.mealz.ui.model.response.MealsCategoriesResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class MealsRepository(private val webService: MealsWebService = MealsWebService()) {
+class MealsRepository(private val webService: MealApi = MealApi()) {
     suspend fun getMeals(): MealsCategoriesResponse {
         return webService.getMeals()
     }
--- app/src/main/java/com/saif/mealz/ui/model/api/MealApi.kt
+++ app/src/main/java/com/saif/mealz/ui/model/api/MealApi.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 
-class MealsWebService {
+class MealApi {
     private var api: MealsApi
 
     init {
